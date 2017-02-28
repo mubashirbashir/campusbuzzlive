10061f62bc7d6e0742c199fe0c75545a28868615
usman <sheikhmh.usman@gmail.com>
2017-03-01 04:04:43 +0530

    Raat k char baje

--- app/src/main/java/com/campusbuzzlive/campusbuzzlive/LoginActivity.java
+++ app/src/main/java/com/campusbuzzlive/campusbuzzlive/LoginActivity.java
@@ -2,6 +2,9 @@ package com.campusbuzzlive.campusbuzzlive;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.support.design.widget.TextInputLayout;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
--- app/src/main/java/com/campusbuzzlive/campusbuzzlive/MainActivity.java
+++ app/src/main/java/com/campusbuzzlive/campusbuzzlive/MainActivity.java
@@ -1,18 +1,30 @@
 package com.campusbuzzlive.campusbuzzlive;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
-
+import android.support.design.widget.TextInputLayout;
+import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
- //   Button bLogin =(Button) findViewById(R.id.bLogin);
-   // Button bSign =(Button) findViewById(R.id.bSign);
+ // Button bLogin =(Button) findViewById(R.id.bLogin);
+  // Button bSign =(Button) findViewById(R.id.bSign);
   //  Button bGuest =(Button) findViewById(R.id.bGuest);
+        Button bSign;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button bSign =(Button) findViewById(R.id.bSign);
+
     }
+
+public void add(View v)
+{
+    Intent abc = new Intent(MainActivity.this,SignupActivity.class);
+    startActivity(abc);
+}
 }
