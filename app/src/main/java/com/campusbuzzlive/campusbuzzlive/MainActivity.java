2acaac5a060cab24ef90fd35416529317170acb5
mubashirbashir <mubashirbashirdar@gmail.com>
2017-02-28 23:19:23 +0530

    Initial commit

---
+++ app/src/main/java/com/campusbuzzlive/campusbuzzlive/MainActivity.java
@@ -0,0 +1,18 @@
+package com.campusbuzzlive.campusbuzzlive;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+ //   Button bLogin =(Button) findViewById(R.id.bLogin);
+   // Button bSign =(Button) findViewById(R.id.bSign);
+  //  Button bGuest =(Button) findViewById(R.id.bGuest);
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
