34e00e49f3501d45c923838ad27065a6d5ce8138
usman <sheikhmh.usman@gmail.com>
2017-03-01 17:00:14 +0530

    submit changed to log in

--- app/src/main/java/com/campusbuzzlive/campusbuzzlive/ForgotPassword.java
+++ app/src/main/java/com/campusbuzzlive/campusbuzzlive/ForgotPassword.java
@@ -1,14 +1,29 @@
 package com.campusbuzzlive.campusbuzzlive;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ForgotPassword extends AppCompatActivity {
+    Button bNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+        Button bNext = (Button) findViewById(R.id.bNext);
 
+
+    }
+
+    public void add(View v)
+
+    {
+        if (v.getId() == R.id.bNext) {
+            Intent next = new Intent(ForgotPassword.this, ChangePassword.class);
+            startActivity(next);
+        }
     }
 }
