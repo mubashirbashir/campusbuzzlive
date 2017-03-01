93c27aa9d80861bfc987329355a1ae0cc7320227
usman <sheikhmh.usman@gmail.com>
2017-03-01 16:28:54 +0530

    cls

--- app/src/main/java/com/campusbuzzlive/campusbuzzlive/LoginActivity.java
+++ app/src/main/java/com/campusbuzzlive/campusbuzzlive/LoginActivity.java
@@ -7,15 +7,18 @@ import android.view.View;
 import android.widget.Button;
 import android.support.design.widget.TextInputLayout;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
     Button bsign;
     Button BLogin;
+    TextView tvForgot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Button bSign = (Button) findViewById(R.id.bSign);
         Button bLogin = (Button) findViewById(R.id.bLogin);
+        TextView tvForgot = (TextView) findViewById(R.id.tvForgot) ;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
@@ -30,6 +33,12 @@ public class LoginActivity extends AppCompatActivity {
         if (v.getId() == R.id.bLogin) {
             Intent Logintent = new Intent(LoginActivity.this, LoginActivity.class);
             startActivity(Logintent);
+
+        }
+        if (v.getId() == R.id.tvForgot) {
+            Intent fp = new Intent(LoginActivity.this,ForgotPassword.class);
+            startActivity(fp);
+
         }
     }
 }
