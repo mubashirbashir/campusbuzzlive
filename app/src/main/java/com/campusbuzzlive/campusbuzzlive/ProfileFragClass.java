cc4ee6dce7a32c7c42fb6c8d5214989b0a5269ce
usman <sheikhmh.usman@gmail.com>
2017-03-11 15:28:33 +0530

    faqqqqq

--- app/src/main/java/com/campusbuzzlive/campusbuzzlive/ProfileFragClass.java
+++ app/src/main/java/com/campusbuzzlive/campusbuzzlive/ProfileFragClass.java
@@ -5,6 +5,15 @@ package com.campusbuzzlive.campusbuzzlive;
  */
 
 import android.content.Intent;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.provider.ContactsContract;
+import android.provider.MediaStore;
+
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -12,35 +21,86 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
 
+
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import java.io.ByteArrayOutputStream;
+
+
 public class ProfileFragClass extends Fragment {
-    Button btn;
+    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1888;
+    Context context;
+    ImageView ivDP;
+    ImageButton imageButton4;
+
     public View onCreateView(LayoutInflater inflater,
                              @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
+
         //Inflate the layout for this fragment
 
-        return inflater.inflate(
+        View rootView = inflater.inflate(
                 R.layout.profile_frag_layout, container, false);
+
+        context = rootView.getContext();
+        imageButton4 = (ImageButton) rootView.findViewById(R.id.ibCamera);
+        ivDP = (ImageView) rootView.findViewById(R.id.ivDP);
+           imageButton4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Pass the context and the Activity class you need to open from the Fragment Class, to the Intent
+
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,
+                        CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+            }
+           });
+    return rootView;
     }
+
     @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == Activity.RESULT_OK) {
+
+                Bitmap bmp = (Bitmap) data.getExtras().get("data");
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+
+                bmp.compress(Bitmap.CompressFormat.PNG, 100, stream);
+                byte[] byteArray = stream.toByteArray();
+
+                // convert byte array to Bitmap
+
+                Bitmap bitmap = BitmapFactory.decodeByteArray(byteArray, 0,
+                        byteArray.length);
+
+
+                ivDP.setImageBitmap(bitmap);
+
+            }
+        }
+    }
+
+        @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+
+
         //you can set the title for your toolbar here for different fragments different titles
         getActivity().setTitle("Profile Settings");
-        Button btn =(Button) view.findViewById(R.id.tbtn);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getActivity(),LoginActivity.class);
-                startActivity(intent);
-            }
-        });
+
+
 
     }
+
+
 }
