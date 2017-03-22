844b33869f8efd9f9d349e08b01dbfb2707624a1
usman <sheikhmh.usman@gmail.com>
2017-03-23 01:10:38 +0530

    Notifications done, pdf opens in googledocs

--- app/src/main/java/com/campusbuzzlive/campusbuzzlive/NotificationFragClass.java
+++ app/src/main/java/com/campusbuzzlive/campusbuzzlive/NotificationFragClass.java
@@ -8,19 +8,58 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 
 
 public class NotificationFragClass extends Fragment {
+    public WebView mWebView;
+    String googleDocs = "https://docs.google.com/viewer?url=";
 
     public View onCreateView(LayoutInflater inflater,
                              @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        //Inflate the layout for this fragment
+        View v= inflater.inflate(R.layout.notification_frag_layout, container, false);
 
-        return inflater.inflate(
-                R.layout.notification_frag_layout, container, false);
+
+
+
+        mWebView = (WebView) v.findViewById(R.id.webview);
+        mWebView.loadUrl("http://www.kashmiruniversity.net/notifications.aspx");
+
+        // Enable Javascript
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        // Force links and redirects to open in the WebView instead of in a browser
+        mWebView.setWebViewClient(new WebViewClient());
+        mWebView.setWebViewClient(new HelloWebViewClient());
+
+        return v;
+
+
     }
+    private class HelloWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView webview, String url){
+            if (url.endsWith(".pdf"))
+            {
+                String googleDocs = "https://docs.google.com/viewer?url=";
+                mWebView.loadUrl(googleDocs + url);
+                return true;
+            }
+            else
+            {
+                // Load all other urls normally.
+                webview.loadUrl(url);
+            }
+            return true;
+        }
+
+    }
+
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
