9555317b9a873aeaa6324ff6ee791f2b3d56932c
mubashirbashir <mubashirbashirdar@gmail.com>
2017-03-30 23:00:49 +0530

    db login  n registration done&maps

--- app/src/main/java/com/campusbuzzlive/campusbuzzlive/MapsActivity.java
+++ app/src/main/java/com/campusbuzzlive/campusbuzzlive/MapsActivity.java
@@ -36,7 +36,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
         AlertDialog.Builder builder = new AlertDialog.Builder(MapsActivity.this);
-          builder.setMessage("Tap on the red marker for more optionslat=!")
+          builder.setMessage("Tap on the red marker for more options!")
                   .setNegativeButton("ok", null)
                   .create()
                     .show();
