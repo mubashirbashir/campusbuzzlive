98e16b12f94b9c62578f095b9c61aea891d92053
usman <sheikhmh.usman@gmail.com>
2017-04-17 22:39:49 +0530

    Mufti ka khwab

--- app/src/main/java/com/campusbuzzlive/campusbuzzlive/ForgotPassword.java
+++ app/src/main/java/com/campusbuzzlive/campusbuzzlive/ForgotPassword.java
@@ -1,6 +1,8 @@
 package com.campusbuzzlive.campusbuzzlive;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -9,7 +11,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class ForgotPassword extends AppCompatActivity {
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class ForgotPassword extends AppCompatActivity implements View.OnClickListener {
 
     Button bNext;
     Button bSearch;
@@ -22,11 +31,11 @@ public class ForgotPassword extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
-        bNext = (Button) findViewById(R.id.bNext);
+
         bSearch=(Button)findViewById(R.id.bSearch);
         etEmailFind=(EditText)findViewById(R.id.etEmailFind);
         etEnrollFind=(EditText)findViewById(R.id.etEnrollFind);
-
+        bSearch.setOnClickListener(this);
         searchU();
 
     }
@@ -89,5 +98,53 @@ public class ForgotPassword extends AppCompatActivity {
     }
 
 
+    @Override
+    public void onClick(View v) {
+        final ProgressDialog progressDialog =new ProgressDialog(this);
+        progressDialog.setTitle("Checking details");
+        progressDialog.setMessage("Please Wait...");
+        progressDialog.setCanceledOnTouchOutside(false);
+        progressDialog.show();
+        Response.Listener<String> responseListener = new Response.Listener<String>() {
 
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonResponse = new JSONObject(response);
+
+
+                    boolean error = jsonResponse.getBoolean("error");
+
+                    if (!error) {
+                        progressDialog.dismiss();
+
+                        AlertDialog.Builder builder =new AlertDialog.Builder(ForgotPassword.this);
+                        builder.setMessage("Please check your email")
+                                .setNegativeButton("ok",null)
+                                .create()
+                                .show();
+                    } else {
+                        progressDialog.dismiss();
+                        String msg =jsonResponse.getString("error_msg");
+                        AlertDialog.Builder builder = new AlertDialog.Builder(ForgotPassword.this);
+                        builder.setMessage(msg)
+                                .setNegativeButton("Retry", null)
+                                .create()
+                                .show();
+
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        ForgotPasswordRequest forgotPasswordRequest = new ForgotPasswordRequest( etEmailFind.getText().toString(), etEnrollFind.getText().toString(), responseListener);
+        RequestQueue queue = Volley.newRequestQueue(ForgotPassword.this);
+        queue.add(forgotPasswordRequest);
+
+
+
+
+    }
 }
---
+++ app/src/main/java/com/campusbuzzlive/campusbuzzlive/ForgotPasswordRequest.java
@@ -0,0 +1,37 @@
+package com.campusbuzzlive.campusbuzzlive;
+
+import com.android.volley.Request;
+import com.android.volley.Request.Method;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by usman on 16-04-2017.
+ */
+
+public class ForgotPasswordRequest extends StringRequest {
+
+    static host h =new host();
+
+    private static final String REGISTER_REQUEST_URL = h.address+"/forgotPassword.php";
+    private Map<String, String> params;
+
+    public ForgotPasswordRequest(String email,String enrollmentid , Response.Listener<String> listener) {
+
+        super(Method.POST, REGISTER_REQUEST_URL, listener, null);
+        params = new HashMap<>();
+        params.put("email", email);
+        params.put("enrollmentid", enrollmentid);
+
+
+
+    }
+
+    @Override
+    public Map<String, String> getParams() {
+        return params;
+    }
+}
