43b92d2833d5b1dcd24d1bd45056f1bd3f2ecd94
usman <sheikhmh.usman@gmail.com>
2017-04-26 11:20:57 +0530

    new

--- app/src/main/java/com/campusbuzzlive/campusbuzzlive/DepartmentsActivity.java
+++ app/src/main/java/com/campusbuzzlive/campusbuzzlive/DepartmentsActivity.java
@@ -1,6 +1,7 @@
 package com.campusbuzzlive.campusbuzzlive;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.widget.SwipeRefreshLayout;
@@ -12,6 +13,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -44,21 +46,47 @@ public class DepartmentsActivity extends AppCompatActivity {
         TextView tvSchoolname =(TextView)findViewById(R.id.schoolName);
         tvSchoolname.setText(schoolName);
         schoolList=new ArrayList<HashMap<String, String>> ();
-        ListView listView = (ListView) findViewById(R.id.lvSchool) ;
-        arrayAdapter= new ArrayAdapter<HashMap<String, String>>(this,R.layout.departments_design,R.id.tvDname){
+        ListView listView = (ListView) findViewById(R.id.lvDepartments) ;
+        arrayAdapter= new ArrayAdapter<HashMap<String, String>>(this,R.layout.departments_design,R.id.tvHODname){
             @NonNull
             @Override
             public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
                 View  v = getLayoutInflater().inflate(R.layout.departments_design,parent,false);
                 TextView tvDname = (TextView) v.findViewById(R.id.tvDname);
                 TextView tvHOD = (TextView) v. findViewById(R.id.tvHODname);
-                TextView tvPhone = (TextView) v.findViewById(R.id.tvPhone);
-                TextView tvEmail = (TextView) v. findViewById(R.id.tvMail);
-                HashMap<String, String> item=getItem(position);
-                tvDname.setText(item.get("DName"));
+                final TextView tvPhone = (TextView) v.findViewById(R.id.tvPhone);
+                final TextView tvEmail = (TextView) v. findViewById(R.id.tvMail);
+                final HashMap<String, String> item=getItem(position);
+                tvDname.setText(item.get("Dname"));
                 tvHOD.setText(item.get("HODname"));
                 tvPhone.setText(item.get("phone"));
                 tvEmail.setText(item.get("email"));
+               tvEmail.setOnClickListener(new View.OnClickListener() {
+                   @Override
+                   public void onClick(View v) {
+                       Intent intent = new Intent(Intent.ACTION_SENDTO);
+                       intent.setType("text/plain");
+                       //intent.setType("message/rfc822");
+                       intent.setData(Uri.parse("mailto:"+tvEmail.getText().toString()));
+
+                       intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                       try {
+                           startActivity(Intent.createChooser(intent, "Send Mail"));
+                       } catch (android.content.ActivityNotFoundException ex) {
+                           Toast.makeText(DepartmentsActivity.this, "No Email Client", Toast.LENGTH_SHORT).show();
+
+                       }
+
+                   }
+               });
+                tvPhone.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent= new Intent((Intent.ACTION_DIAL));
+                        intent.setData(Uri.parse("tel:"+tvPhone.getText().toString()));
+                        startActivity(intent);
+                    }
+                });
                 return v;
             }
         } ;
